8c37eaf5ca39b70ac97351bda247ac0c1159251c
shadialahmd <shadi.alahmd@gmail.com>
2021-03-08 12:05:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 
 
-function getusers(){
-    try {
-        fetch('https://reqres.in/api/users?page=3')
+// function getusers(){
+//     try {
+//         fetch('https://reqres.in/api/users?page=3')
+//         .then(res=> res.json())
+//         .then(data => console.log(data))
+        
+//     } catch (error) {
+//         console.log('ee'+error)
+//     }
+
+// }
+
+// getusers();
+
+let num=12;
+
+function getsingluser(){
+
+for(i=1;i<=num;i++){
+
+ try {
+        fetch('https://reqres.in/api/users/'+i)
         .then(res=> res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+        })
         
     } catch (error) {
         console.log('ee'+error)
     }
+}
+    
 
 }
-
-getusers();
+getsingluser();
