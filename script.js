dcae8516641b6d161120b8bbc8ced4c8e401d42c
shadialahmd <shadi.alahmd@gmail.com>
2021-03-08 11:56:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
 
 function getusers(){
-    fetch('https://reqres.in/api/users')
-    .then(res=> res.json())
-    .then(data => console.log(data))
+    try {
+        fetch('https://reqres.in/api/users?page=3')
+        .then(res=> res.json())
+        .then(data => console.log(data))
+        
+    } catch (error) {
+        console.log('ee'+error)
+    }
+
 }
 
 getusers();
